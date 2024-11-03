c334fe01b77a5e6bfee1385204eddfad6922b85a
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2024-11-02 21:11:15 -0700

    chore: add missing rewrite

--- Leanwuzla/Aux.lean
+++ Leanwuzla/Aux.lean
@@ -23,4 +23,7 @@ namespace BitVec
 @[bv_normalize] protected def sge {n : Nat} (x y : BitVec n) : Bool :=
   BitVec.sle y x
 
+@[bv_normalize] theorem BitVec.sshiftRight'_eq (x : BitVec w) (n : BitVec v) : x.sshiftRight' n = x.sshiftRight n.toNat :=
+  rfl
+
 end BitVec
