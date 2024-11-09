5f6ffc029bac06841d135767a33249f8ca533814
Henrik Böving <hargonix@gmail.com>
2024-11-09 15:58:45 +0100

    chore: adapt bv_compare

--- Test/BVCompare.lean
+++ Test/BVCompare.lean
@@ -32,7 +32,21 @@ theorem extracted_1 (x y : BitVec 8) : x + y = x + x := by
   bv_compare
   sorry
 
-set_option sat.timeout 1
+set_option sat.timeout 1 in
 example (x y : BitVec 64) : x * y = y * x := by
   bv_compare
   sorry
+
+example (a : BitVec 32) (b : a + 4294967295#32 < 32#32) (c : 32#32 < a) : False := by
+  bv_decide
+
+
+set_option sat.timeout 120
+example :
+    a2 - a1 < b1 - a1 → a2 - a1 < b2 - a1 →
+    b2 - b1 < a1 - b1 → b2 - b1 < a2 - b1 →
+    b2 - b1 = 18446744073709551615#64 ∨ c2 - b1 ≤ b2 - b1 ∧ c1 - b1 ≤ c2 - b1 →
+    ((a2 - a1 < c1 - a1 ∧ a2 - a1 < c2 - a1)
+    ∧ c2 - c1 < a1 - c1)
+    ∧ c2 - c1 < a2 - c1 := by
+  bv_compare
